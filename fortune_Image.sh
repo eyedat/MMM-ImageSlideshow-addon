ed7092ccf6e8fb7b8292837b963649fb997c8a7a
eyeDAT <eyedat@gmail.com>
2022-01-26 06:15:44 +0100

    Update fortune_Image.sh

--- fortune_Image.sh
+++ fortune_Image.sh
@@ -129,215 +129,152 @@ Grosse2=80 #26
 #     0,63% stilblueten
 #     0,16% huhn
 #     0,32% murphy
+#
 # Aldi Kamera "Das Modell Maginon IPC-100AC" Bild import start ***********************************************************************************************************************************************
+
 BENUTZER="admin" 
 PASSWORT="text"
 CAMIP="192.168.1.3" # IP der Maginon IPC-100AC
 
 # Bild holden und conventieren in png
-wget -O $pfad/exampleImages_null/snapshot.cgi http://$BENUTZER:$PASSWORT@$CAMIP/snapshot.cgi && convert $pfad/exampleImages_null/snapshot.cgi $pfad/exampleImages_null/_cam.png
 
+wget -O $pfad/exampleImages_null/snapshot.cgi http://$BENUTZER:$PASSWORT@$CAMIP/snapshot.cgi && convert $pfad/exampleImages_null/snapshot.cgi $pfad/exampleImages_null/_cam.png
 composite -gravity Center $pfad/exampleImages_null/_cam.png $pfad/exampleImages_null/_Basis.gif $pfad/exampleImages_null/Fotos_Kamera.png
 
 # Beschriftung
+
 convert $pfad/exampleImages_null/Fotos_Kamera.png -gravity South -font $SCHRIFT2 -pointsize 50 -fill yellow -draw "text 0,0 'IP der Maginon IPC-100AC'" $pfad/exampleImages_null/Fotos_1.png
 
 # Löschen der Bearbeitungsdatein
+
 sleep 1 && rm $pfad/exampleImages_null/snapshot.cgi && rm $pfad/exampleImages_null/_cam.png && rm $pfad/exampleImages_null/Fotos_Kamera.png
 
 # Kamera import ende      ***********************************************************************************************************************************************
+#
 # Bild des Tages import start ***********************************************************************************************************************************************
 # news
 
-
 wget -O $pfad/exampleImages_null/APTOPIX-Germany-Daily-Life.jpg $news && convert $pfad/exampleImages_null/APTOPIX-Germany-Daily-Life.jpg $pfad/exampleImages_null/_bilddestages.png
-
 composite -gravity Center $pfad/exampleImages_null/_bilddestages.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_Tages.png
-
-# Beschriftung
 convert $pfad/exampleImages_null/Fotos_Tages.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Bild des Tages'" $pfad/exampleImages_null/Fotos_2.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/APTOPIX-Germany-Daily-Life.jpg && rm $pfad/exampleImages_null/_bilddestages.png && rm $pfad/exampleImages_null/Fotos_Tages.png
 
 # Bild des Tages import ende      
+#
 # Bild Paris import start 
 # Paris
 
 wget -O $pfad/exampleImages_null/paris.jpg $paris && convert $pfad/exampleImages_null/paris.jpg $pfad/exampleImages_null/_paris.png
-
 composite -gravity Center $pfad/exampleImages_null/_paris.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_paris.png
-
 convert $pfad/exampleImages_null/Fotos_paris.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Paris'" $pfad/exampleImages_null/Fotos_3.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/paris.jpg && rm $pfad/exampleImages_null/_paris.png && rm $pfad/exampleImages_null/Fotos_paris.png
 
 # Bild Paris import ende  
 # Bild Ahlbeck import start 
 # Ahlbeck 
 
-
-
 wget -O $pfad/exampleImages_null/Ahlbeck.jpg $Ahlbeck && convert $pfad/exampleImages_null/Ahlbeck.jpg $pfad/exampleImages_null/_Ahlbeck.png
-
 composite -gravity Center $pfad/exampleImages_null/_Ahlbeck.png $pfad/exampleImages_null/_Basis3.gif $pfad/exampleImages_null/Fotos_Ahlbeck.png
-
 convert $pfad/exampleImages_null/Fotos_Ahlbeck.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Ahlbeck'" $pfad/exampleImages_null/Fotos_4.png
-
-
-
 sleep 2 && rm $pfad/exampleImages_null/Ahlbeck.jpg && rm $pfad/exampleImages_null/_Ahlbeck.png && rm $pfad/exampleImages_null/Fotos_Ahlbeck.png
 
 # Bild Ahlbeck import ende      
 # Bild Hamburg import start 
 # Hamburg 
 
-
 wget -O $pfad/exampleImages_null/Hamburg.jpg $Hamburg && convert $pfad/exampleImages_null/Hamburg.jpg $pfad/exampleImages_null/_Hamburg.png
-
 composite -gravity Center $pfad/exampleImages_null/_Hamburg.png $pfad/exampleImages_null/_Basis4.gif $pfad/exampleImages_null/Fotos_Hamburg.png
-
 convert $pfad/exampleImages_null/Fotos_Hamburg.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Hamburg'" $pfad/exampleImages_null/Fotos_5.png
-
 sleep 2 && rm $pfad/exampleImages_null/Hamburg.jpg && rm $pfad/exampleImages_null/_Hamburg.png && rm $pfad/exampleImages_null/Fotos_Hamburg.png
 
 # Bild Hamburg import ende  
 # Bild Bremen import start 
 # Bremen 
 
-
 wget -O $pfad/exampleImages_null/Bremen.jpg $Bremen && convert $pfad/exampleImages_null/Bremen.jpg $pfad/exampleImages_null/_Bremen.png
-
 composite -gravity Center $pfad/exampleImages_null/_Bremen.png $pfad/exampleImages_null/_Basis4.gif $pfad/exampleImages_null/Fotos_Bremen.png
-
 convert $pfad/exampleImages_null/Fotos_Bremen.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Bremen'" $pfad/exampleImages_null/Fotos_6.png
-
 sleep 2 && rm $pfad/exampleImages_null/Bremen.jpg && rm $pfad/exampleImages_null/_Bremen.png && rm $pfad/exampleImages_null/Fotos_Bremen.png
 
 # Bild Bremen import ende  
 # Bild Berlin import start 
 # Berlin 
 
-
 wget -O $pfad/exampleImages_null/Berlin.jpg $Berlin && convert $pfad/exampleImages_null/Berlin.jpg $pfad/exampleImages_null/_Berlin.png
-
 composite -gravity Center $pfad/exampleImages_null/_Berlin.png $pfad/exampleImages_null/_Basis4.gif $pfad/exampleImages_null/Fotos_Berlin.png
-
 convert $pfad/exampleImages_null/Fotos_Berlin.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Berlin'" $pfad/exampleImages_null/Fotos_7.png
-
 sleep 2 && rm $pfad/exampleImages_null/Berlin.jpg && rm $pfad/exampleImages_null/_Berlin.png && rm $pfad/exampleImages_null/Fotos_Berlin.png
 
 # Bild Berlin import ende  
 # Bild Kiel import start 
 # Kiel 
 
-
 wget -O $pfad/exampleImages_null/Kiel.jpg $Kiel && convert $pfad/exampleImages_null/Kiel.jpg $pfad/exampleImages_null/_Kiel.png
-
 composite -gravity Center $pfad/exampleImages_null/_Kiel.png $pfad/exampleImages_null/_Basis4.gif $pfad/exampleImages_null/Fotos_Kiel.png
-
 convert $pfad/exampleImages_null/Fotos_Kiel.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Kiel'" $pfad/exampleImages_null/Fotos_8.png
-
 sleep 2 && rm $pfad/exampleImages_null/Kiel.jpg && rm $pfad/exampleImages_null/_Kiel.png && rm $pfad/exampleImages_null/Fotos_Kiel.png
 
 # Bild Kiel import ende  
 # Bild Mallorca import start 
 # Mallorca 
 
-
 wget -O $pfad/exampleImages_null/Mallorca.jpg $Mallorca && convert $pfad/exampleImages_null/Mallorca.jpg $pfad/exampleImages_null/_Mallorca.png
-
 composite -gravity Center $pfad/exampleImages_null/_Mallorca.png $pfad/exampleImages_null/_Basis4.gif $pfad/exampleImages_null/Fotos_Mallorca.png
-
 convert $pfad/exampleImages_null/Fotos_Mallorca.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Mallorca'" $pfad/exampleImages_null/Fotos_9.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/Mallorca.jpg && rm $pfad/exampleImages_null/_Mallorca.png && rm $pfad/exampleImages_null/Fotos_Mallorca.png
 
 # Bild Mallorca import ende  
 # Bild Oldenburg import start 
 # Oldenburg
 
-
 wget -O $pfad/exampleImages_null/Oldenburg.jpg $Oldenburg && convert $pfad/exampleImages_null/Oldenburg.jpg $pfad/exampleImages_null/_Oldenburg.png
-
 composite -gravity Center $pfad/exampleImages_null/_Oldenburg.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_Oldenburg.png
-
 convert $pfad/exampleImages_null/Fotos_Oldenburg.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Oldenburg'" $pfad/exampleImages_null/Fotos_10.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/Oldenburg.jpg && rm $pfad/exampleImages_null/_Oldenburg.png && rm $pfad/exampleImages_null/Fotos_Oldenburg.png
 
 # Bild Oldenburg import ende  
 # Bild Dangast import start 
 # Dangast
 
-
 wget -O $pfad/exampleImages_null/Dangast.jpg $Dangast && convert $pfad/exampleImages_null/Dangast.jpg $pfad/exampleImages_null/_Dangast.png
-
 composite -gravity Center $pfad/exampleImages_null/_Dangast.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_Dangast.png
-
 convert $pfad/exampleImages_null/Fotos_Dangast.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Varel Dangast'" $pfad/exampleImages_null/Fotos_11.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/Dangast.jpg && rm $pfad/exampleImages_null/_Dangast.png && rm $pfad/exampleImages_null/Fotos_Dangast.png
 
 # Bild Dangast import ende  
 # Bild Friedrichskoog import start 
 # Friedrichskoog
 
-
 wget -O $pfad/exampleImages_null/Friedrichskoog.jpg $Friedrichskoog && convert $pfad/exampleImages_null/Friedrichskoog.jpg $pfad/exampleImages_null/_Friedrichskoog.png
-
 composite -gravity Center $pfad/exampleImages_null/_Friedrichskoog.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_Friedrichskoog.png
-
 #convert $pfad/exampleImages_null/Fotos_Friedrichskoog.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Nordseeheilbad Friedrichskoog'" $pfad/exampleImages_null/Fotos_12.png
-
 convert $pfad/exampleImages_null/Fotos_Friedrichskoog.png -gravity South -font $SCHRIFT2 -pointsize 60 -fill yellow -draw "text 0,0 'Nordseeheilbad Friedrichskoog'" $pfad/exampleImages_null/Fotos_12.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/Friedrichskoog.jpg && rm $pfad/exampleImages_null/_Friedrichskoog.png && rm $pfad/exampleImages_null/Fotos_Friedrichskoog.png
 
 # Bild Friedrichskoog import ende  
 # Bild Wien import start 
 # Wien
 
-
 wget -O $pfad/exampleImages_null/Wien.jpg $Wien && convert $pfad/exampleImages_null/Wien.jpg $pfad/exampleImages_null/_Wien.png
-
 composite -gravity Center $pfad/exampleImages_null/_Wien.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_Wien.png
-
 convert $pfad/exampleImages_null/Fotos_Wien.png -gravity South -font $SCHRIFT2 -pointsize $Grosse2 -fill yellow -draw "text 0,0 'Wien'" $pfad/exampleImages_null/Fotos_13.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/Wien.jpg && rm $pfad/exampleImages_null/_Wien.png && rm $pfad/exampleImages_null/Fotos_Wien.png
 
 # Bild Wien import ende  
 # Bild Heidelberg import start 
 # Heidelberg
 
-
 wget -O $pfad/exampleImages_null/Heidelberg.jpg $Heidelberg && convert $pfad/exampleImages_null/Heidelberg.jpg $pfad/exampleImages_null/_Heidelberg.png
-
 composite -gravity Center $pfad/exampleImages_null/_Heidelberg.png $pfad/exampleImages_null/_Basis2.gif $pfad/exampleImages_null/Fotos_Heidelberg.png
-
 convert $pfad/exampleImages_null/Fotos_Heidelberg.png -gravity South -font $SCHRIFT2 -pointsize 70 -fill yellow -draw "text 0,0 'Heidelberg - Schlossberg'" $pfad/exampleImages_null/Fotos_14.png
-
-
 sleep 2 && rm $pfad/exampleImages_null/Heidelberg.jpg && rm $pfad/exampleImages_null/_Heidelberg.png && rm $pfad/exampleImages_null/Fotos_Heidelberg.png
 
 # Bild Heidelberg import ende   ***********************************************************************************************************************************************
-
-
 # Das eigentliche Script:
 
 zaehler=1                                 #zählt von z.B. 1
 BIS=$(ls $pfad/exampleImages_null | grep -c Fotos_)  #zählt von z.B. 22
 TEXT=$(cat $pfad/TEXT_des_Tages.txt)
-
-
-
               
 while [ $zaehler -le $BIS ]  #zählt bis z.B. 22 Fotos_1 - 22
 do # /usr/games/fortune $fortune 
@@ -347,14 +284,10 @@ sleep 0.1
 
 convert $pfad/exampleImages_null/Fotos_$zaehler.png -gravity North -font "$SCHRIFT" -pointsize $Grosse -fill yellow -draw "text 0,0 '$TEXT'" $pfad/exampleImages/Fotos_$zaehler.png
 
-
 echo "fortune $fortune > $pfad/TEXT_des_Tages.txt"
 echo "convert $pfad/exampleImages_null/Fotos_"$zaehler".png"
 echo
 
 zaehler=`expr $zaehler + 1`    # +2 wäre jeder zweiter
 done
-
-
-
 exit 0
