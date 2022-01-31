422e257769ca50b19717cef79d499f7ba47e97a2
eyeDAT <eyedat@gmail.com>
2022-01-31 12:24:43 +0100

    Update umbenennen_TOOL_von_XY_png_zu_Foto_Zahl_png.sh

--- exampleImages_null/umbenennen_TOOL_von_XY_png_zu_Foto_Zahl_png.sh
+++ exampleImages_null/umbenennen_TOOL_von_XY_png_zu_Foto_Zahl_png.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# zum vorbereiten fürs Mirror-script und Umbenenn von zufälligen png Dateinamen
+# Zum vorbereiten für das "" Script und Umbenenn von zufälligen Namen xy.png in Foto_zahl.png
 PWD=`pwd`
 
-ab=16 # wenn Die erste Datei aus dem script produziert Fotos_16.png heisen wird und die Eigenen Bilder 17 aufwärts werden sollen...
+ab=16 # Wenn die erste Datei aus dem script "Fotos_16.png" produziert wird
+#       Und die Eigenen Bilder ab 17 aufwärts werden sollen, sollte ab=16 sein.
 
 for f in *.png; do 
 ab=`expr $ab + 1`
